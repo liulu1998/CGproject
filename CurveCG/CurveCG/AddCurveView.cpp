691cc633bac6a3f1403fadeb95eef7ae595b61ad
liulu1998 <1274272851@qq.com>
2020-05-17 18:25:10 +0800

    feat(addcurveview): add functions handle messages in AddCurveView dialog

--- CurveCG/CurveCG/AddCurveView.cpp
+++ CurveCG/CurveCG/AddCurveView.cpp
@@ -24,10 +24,14 @@ void AddCurveView::DoDataExchange(CDataExchange* pDX)
 {
 	CFormView::DoDataExchange(pDX);
 	DDX_Control(pDX, IDC_BUTTON1, m_addCurveDlg);
+	DDX_Control(pDX, IDC_COMBO_DEGREE, m_degree_cbx);
 }
 
 BEGIN_MESSAGE_MAP(AddCurveView, CFormView)
 	ON_WM_SIZE()
+	ON_BN_CLICKED(IDC_RADIO_BEZIER, &AddCurveView::OnBnClickedRadioBezier)
+	ON_BN_CLICKED(IDC_RADIO_BSPLINE, &AddCurveView::OnBnClickedRadioBspline)
+	ON_BN_CLICKED(IDC_BUTTON_ADDCURVE, &AddCurveView::OnBnClickedButtonAddcurve)
 END_MESSAGE_MAP()
 
 
@@ -87,4 +91,99 @@ CView* AddCurveView::GetView(CRuntimeClass* pClass) {
 		}
 	}
 	return NULL;
+}
+
+
+
+/*************************************************
+Function:		OnInitialUpdate
+Description:	初始化 增加曲线 Dialog
+Author:			刘陆
+Calls:
+Input:
+Return:
+*************************************************/
+void AddCurveView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+
+	// 初始化 次数 下拉框
+	CString degrees[] = { TEXT("1"), TEXT("2"), TEXT("3") };
+
+	for (int i = 0; i < 3; i++)
+		m_degree_cbx.AddString(degrees[i]);
+
+	m_degree_cbx.SetCurSel(2);		// 默认三次
+
+	// 处理 单选框, 默认选中 "B-样条曲线"
+	CButton* radio = (CButton*)GetDlgItem(IDC_RADIO_BSPLINE);
+	radio->SetCheck(1);
+
+	// TODO: 初始化曲线精度
+}
+
+
+/*************************************************
+Function:		OnBnClickedRadioBezier
+Description:	处理 单选框Bezier 选中事件
+Author:			刘陆
+Calls:
+Input:
+Return:
+*************************************************/
+void AddCurveView::OnBnClickedRadioBezier()
+{
+	this->selectedType = Bezier;
+}
+
+
+/*************************************************
+Function:		OnBnClickedRadioBezier
+Description:	处理 单选框B-样条 选中事件
+Author:			刘陆
+Calls:
+Input:
+Return:
+*************************************************/
+void AddCurveView::OnBnClickedRadioBspline()
+{
+	this->selectedType = Spline;
+}
+
+
+/*************************************************
+Function:		OnBnClickedButtonAddcurve
+Description:	处理 "添加曲线" 按钮点击事件
+Author:			刘陆
+Calls:
+Input:
+Return:
+*************************************************/
+void AddCurveView::OnBnClickedButtonAddcurve()
+{
+	// 次数下拉框
+	int idx = m_degree_cbx.GetCurSel();
+	CString str;
+	m_degree_cbx.GetLBText(idx, str);
+
+	int curDegree = _ttoi(str);		// 曲线次数 转为 整型
+
+	// TODO: 获取精度
+	double precision = 0.1;
+
+	// 获取 DrawView 指针
+	CRuntimeClass* pClass = RUNTIME_CLASS(DrawView);
+	DrawView* pDraw = (DrawView*)GetView(pClass);
+
+	// DrawView 中新增一条曲线
+	pDraw->addCurve(this->selectedType, curDegree, precision);
+
+	// TODO: CurveInfoView 中 更新信息
+
+	// 调试信息
+	//int curvesNum = pDraw->getCurvesNum();
+
+	//CString text;
+	//text.Format(TEXT("%d,  %c,  %d"), curDegree, this->selectedType, curvesNum);
+	//MessageBox(text);
 }
